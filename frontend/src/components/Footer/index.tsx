e69fedfe760cfc25a0037c54a386820849c9e583
Gersey Stelmach <gerseystelmach@gmail.com>
2021-11-01 15:26:03 +0100

    Translating footer to english

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -2,9 +2,9 @@ const Footer = () => {
     return (
         <footer className="footer mt-auto py-3 bg-dark">
             <div className="container">
-                <p className="text-light">App desenvolvido por <a href="https://github.com/gerseystelmach" target="_blank" rel="noreferrer">Gersey Stelmach</a></p>
+                <p className="text-light">App developed by <a href="https://github.com/gerseystelmach" target="_blank" rel="noreferrer">Gersey Stelmach</a></p>
                 <p className="text-light"><small><strong>Semana Spring React</strong><br />
-                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+                Event promoted by the DevSuperior school: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
             </div>
         </footer>
     );
