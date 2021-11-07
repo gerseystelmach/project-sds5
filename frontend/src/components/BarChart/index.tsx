b0662ad71b9e1c626d09922ef204f5cadb64f366
Gersey Stelmach <gerseystelmach@gmail.com>
2021-11-07 10:36:16 +0100

    BarChart integration

--- frontend/src/components/BarChart/index.tsx
+++ frontend/src/components/BarChart/index.tsx
@@ -1,34 +1,88 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
+import { SaleSuccess } from 'types/sale';
+import { round } from 'utils/format';
+import { BASE_URL } from 'utils/requests';
+
+type SeriesData = {
+    name: string;
+    data: number[];
+}
+type ChartData = {
+    labels: {
+        categories: string[];
+    };
+    series: SeriesData[];
+}
+
 const BarChart = () => {
 
-    const options = {
-        plotOptions: {
-            bar: {
-                horizontal: true,
-            }
-        },
-    };
-    
-    const mockData = {
+    const [chartData, setChartData] = useState<ChartData>({
         labels: {
-            categories: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
+            categories: []
         },
         series: [
             {
-                name: "% Sucesso",
-                data: [43.6, 67.1, 67.7, 45.6, 71.1]                   
+                name: "",
+                data: []
             }
         ]
-    };
-    return (
+    });
 
-/*  Chart é um componente criado pela livraria ApexCharts. Preciso indicar os parâmetros necessários para que o chart seja criado. Source: https://apexcharts.com/docs/react-charts/  */
-     <Chart options={{...options, xaxis: mockData.labels}}
-            series={mockData.series}
-            type="bar"
-            height="240"
-     />
-    );
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales/success-by-seller`)
+            .then(response => {
+                // Puxando dados da Api, confrome tipos definidos no arquivo sale.ts
+                const data = response.data as SaleSuccess[];
+                // Filtrando dados da API e criando uma nova coleção para aplicar no chart 
+                const myLabels = data.map(x => x.sellerName);
+                const mySeries = data.map(x => round(100.0 * x.deals / x.visited, 1));
+
+                setChartData({
+                    labels: {
+                        categories: myLabels
+                    },
+                    series: [
+                        {
+                            name: "% Success",
+                            data: mySeries
+                        }
+                    ]
+                });
+            });
+    }, []);
+
+/* const mockData = {
+    labels: {
+        categories: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
+    },
+    series: [
+        {
+            name: "% Sucesso",
+            data: [43.6, 67.1, 67.7, 45.6, 71.1]
+        }
+    ]
+}; */
+
+const options = {
+    plotOptions: {
+        bar: {
+            horizontal: true,
+        }
+    },
+};
+
+return (
+
+    /*  Chart é um componente criado pela livraria ApexCharts. Preciso indicar os parâmetros necessários para que o chart seja criado. Source: https://apexcharts.com/docs/react-charts/  */
+    <Chart options={{ ...options, xaxis: chartData.labels }}
+        series={chartData.series}
+        type="bar"
+        height="240"
+    />
+);
 }
 
 export default BarChart;
--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,5 +1,11 @@
 /* Definindo tipos dos dados a serem coletados do API */
 export type SaleSum = {
-    sellerName: string,
+    sellerName: string;
     sum: number;
+}
+
+export type SaleSuccess = {
+    sellerName: string;
+    visited: number;
+    deals: number;
 }
